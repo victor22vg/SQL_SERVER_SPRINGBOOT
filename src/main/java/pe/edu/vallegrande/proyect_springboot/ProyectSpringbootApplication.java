abd64613699cefb2e8652f6951719237ecaec20a
Victor Torreblanca <victor.torreblanca.franco@vallegrande.edu.pe>
2025-03-18 14:54:54 +0000

    SPRING BOOT

---
+++ src/main/java/pe/edu/vallegrande/proyect_springboot/ProyectSpringbootApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande.proyect_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/vallegrande/proyect_springboot/ProyectSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande.proyect_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
